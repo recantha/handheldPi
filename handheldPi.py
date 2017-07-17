0164cec9aad8754e353b3cf8c84f2d774c9d6efd
Michael Horne <mike@recantha.co.uk>
2017-07-17 12:21:22 +0000

    Change to use CPUTemperature from GPIO Zero. Add PingServer to keep track of The Case Centre servers

--- handheldPi.py
+++ handheldPi.py
@@ -5,7 +5,7 @@ import math
 import commands
 import os
 import Adafruit_DHT
-from gpiozero import LED, Button
+from gpiozero import LED, Button, CPUTemperature, PingServer
 from wifi import Cell, Scheme
 from wireless import Wireless
 
@@ -20,15 +20,19 @@ low_battery = Button(10)
 
 message_speed = 0.2
 
+hosts_to_monitor = ['www.thecasecentre.org','admin.thecasecentre.org']
+
 wireless = Wireless()
 
 # Scroll a message across the display
-def show_message(message):
+def show_message(message, current_operation):
+    global operation
+
     pos = 0
 
     print(message)
 
-    while pos < len(message):
+    while pos < len(message) and operation == current_operation:
         display.clear()
         display.print_str(message[pos:pos+4])
         display.write_display()
@@ -52,9 +56,8 @@ def shutdown():
     os.system("sudo halt")
 
 def getCPUtemperature():
-    res = os.popen("vcgencmd measure_temp").readline()
-    temp_C = res.replace("temp=","").replace("'C\n", "")
-    temp_C = int(float(temp_C))
+    cpu = CPUTemperature()
+    temp_C = int(float(cpu.temperature))
     temp_F = round(9.0/5.0*temp_C+32)
     temp_C = round(temp_C)
 
@@ -80,43 +83,64 @@ def scanForCells():
     return cells
 
 # Start conditions
+# Turn the blue LED on
 blue_led.on()
+# When button is pressed, move to next operation
 button.when_pressed = increaseOperation
+# When button is held for 5 seconds, trigger the shutdown
 button.hold_time = 5
 button.when_held = shutdown
+# When low battery alert goes high, trigger the shutdown
 low_battery.when_pressed = shutdown
 
-operation = 0
+operation = 6
 
 while True:
     if operation == 0:
         hostname = socket.gethostname()
-        show_message(hostname)
+        show_message(hostname, operation)
 
     elif operation == 1:
         current_ap = wireless.current()
-        show_message("Connected to %s" % current_ap)
+        show_message("Connected to %s" % current_ap, operation)
 
     elif operation == 2:
         ip_addresses = readIPaddresses()
 
         for addr in ip_addresses:
-            show_message(addr)
+            show_message(addr, operation)
 
     elif operation == 3:
         # DHT11 on pin 23
         humidity, temperature = Adafruit_DHT.read_retry(11, 23)
-        show_message('Temp {0:0.1f}C  Humid {1:0.1f}%'.format(temperature, humidity))
+        show_message('Temp {0:0.1f}C  Humid {1:0.1f}%'.format(temperature, humidity), operation)
 
     elif operation == 4:
         temp_C, temp_F = getCPUtemperature()
-        show_message("CPU {}C / {}F".format(temp_C, temp_F))
+        show_message("CPU {}C / {}F".format(temp_C, temp_F), operation)
 
     elif operation == 5:
-        show_message('Wifi Networks')
+        show_message('Wifi Networks', operation)
         cells = scanForCells()
         for cell in cells:
-            show_message(cell.summary)
+            show_message(cell.summary, operation)
+
+    elif operation == 6:
+        blue_led.off()
+        for host in hosts_to_monitor:
+            pinger = PingServer(host)
+            if pinger.value:
+                status = "UP"
+                blue_led.on()
+            else:
+                status = "DOWN"
+                blue_led.blink()
+
+            show_message("Ping to {} - {}".format(host, status), operation)
+
+            for i in range(0,20):
+                if operation == 6:
+                    time.sleep(1)
 
     else:
         operation = 0
